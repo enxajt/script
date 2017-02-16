e837067dd329823ddd8190947c882ef361197e1b
root <root@desktop-VM>
2017-02-16 18:09:11 +0100

    works

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 gsettings set org.gnome.settings-daemon.plugins.keyboard active false
 gsettings set org.mate.caja.desktop home-icon-visible false
 gsettings set org.mate.caja.desktop volumes-visible false
