e0935b3a561c80d3ff507788db432ac8e115b1b7
enxajt <enxajt@gmail.com>
2017-02-16 18:22:52 +0100

    try

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 gsettings set org.gnome.settings-daemon.plugins.keyboard active false
+gsettings set org.mate.background picture-filename /home/enxajt/black.jpg
 gsettings set org.mate.caja.desktop home-icon-visible false
 gsettings set org.mate.caja.desktop volumes-visible false
