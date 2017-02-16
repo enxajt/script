414f74f81648d8f9b0dabb96066bf77762806668
enxajt <enxajt@gmail.com>
2017-02-16 17:46:16 +0100

    try

--- up-terminal.sh
+++ up-terminal.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 gnome-terminal --geometry 100*100
 wmctrl -r "Terminal" -e 0,0,0,800,1200
+#gconftool-2 --type string --set /desktop/gnome/applications/terminal/exec 'gnome-terminal --geometry=120x40'
+"Mozilla Firefox" --geometry 100*100
+wmctrl -r "Mozilla Firefox" -e 0,1100,0,720,920
