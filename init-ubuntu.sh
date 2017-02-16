e521f8b9025daec76f89b7ea7ba580cd2dae7903
enxajt <enxajt@gmail.com>
2017-02-16 18:04:01 +0100

    try

---
+++ init-ubuntu.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.settings-daemon.plugins.keyboard active false
+gsettings set org.mate.caja.desktop home-icon-visible false
+gsettings set org.mate.caja.desktop volumes-visible false
