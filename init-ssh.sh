0619834e56c47856a3cd70816f8e989941304903
enxajt <enxajt@gmail.com>
2017-03-17 18:25:28 +0100

    cut .config from path

--- init-ssh.sh
+++ init-ssh.sh
@@ -6,6 +6,6 @@ ln -sf /vagrant/shared/id_rsa ~/.ssh/id_rsa
 touch ~/.ssh/known_hosts
 
 ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
-[ ! -e ~/.confi/vimfx ~/.config/private-config] && git clone git@bitbucket.org:enxajt/private-config.git ~/.config/private-config
-sh ~/.config/private-config/git.sh
-#sh ~/.config/private-config/user.sh
+[ ! -e ~/.private-config] && git clone git@bitbucket.org:enxajt/private-config.git ~/.private-config || echo already existing
+sh ~/.private-config/git.sh
+#sh ~/.private-config/user.sh
