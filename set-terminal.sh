b50e4c018dfbe4c87be636c367dc60d0ff0c5f5d
enxajt <enxajt@gmail.com>
2017-03-19 14:01:20 +0100

    sth

--- set-terminal.sh
+++ set-terminal.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 link=/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9
 dconf write $link/use-transparent-background true
-dconf write $link/background-transparency-percent '5'
+dconf write $link/background-transparency-percent '10'
+#dconf write $link/background-transparency-percent '5'
 #dconf write $link/use-transparent-background false
