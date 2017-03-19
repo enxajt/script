4aa2fd593aad020b6b56cc839efe27d53ceedd82
enxajt <enxajt@gmail.com>
2017-03-19 10:22:44 +0100

    add knownhosts

--- init-ssh.sh
+++ init-ssh.sh
@@ -2,8 +2,8 @@
 
 # Copy over private key, and set permissions
 mkdir -p ~/.ssh/
-ln -sf /vagrant/shared/id_rsa ~/.ssh/id_rsa
-touch ~/.ssh/known_hosts
+ln -sf /vagrant/shared/.ssh/id_rsa ~/.ssh/id_rsa
+ln -sf /vagrant/shared/.ssh/known_hosts ~/.ssh/known_hosts
 
 ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
 [ ! -e ~/.private-config ] && git clone git@bitbucket.org:enxajt/private-config.git ~/.private-config || echo already existing
