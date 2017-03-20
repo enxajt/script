8ad6261662c897f4424a237126c52daa156c7f77
enxajt <enxajt@gmail.com>
2017-03-20 13:25:52 +0100

    sht

--- init-ssh.sh
+++ init-ssh.sh
@@ -8,6 +8,8 @@ ln -sf /vagrant/shared/.ssh/known_hosts ~/.ssh/known_hosts
 cp /vagrant/shared/.ssh/config_linux .ssh/config
 
 ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
-[ ! -e ~/.private-config ] && git clone git@bitbucket.org:enxajt/private-config.git ~/.private-config || echo already existing
+#[ ! -e ~/.private-config ] && git clone git@bitbucket.org:enxajt/private-config.git ~/.private-config || echo already existing
+[ -e ~/.private-config ] && mv ~/.private-config ~/.private-config.bak
+ln -s /vagrant/shared/.private-config ~/.private-config
 sh ~/.private-config/git.sh
 #sh ~/.private-config/user.sh
