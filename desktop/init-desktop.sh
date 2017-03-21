caefd71936c9d5474f00208831b3aa037bea78c0
enxajt <enxajt@gmail.com>
2017-03-21 16:22:53 +0100

    ~ > /home/enxajt/

--- desktop/init-desktop.sh
+++ desktop/init-desktop.sh
@@ -4,8 +4,8 @@
 convert -size 500x500 xc:black ~/.black.jpg
 #export DISPLAY=:0.0
 #gsettings set org.mate.background picture-filename ~/.black.jpg
-dconf write /org/mate/desktop/background/picture-filename "'~/.black.jpg'"
-sleep 1 && rm ~/.black.jpg
+dconf write /org/mate/desktop/background/picture-filename "'$HOME/.black.jpg'"
+#sleep 1 && rm ~/.black.jpg
 
 # desktop icon
 #gsettings set org.mate.caja.desktop home-icon-visible false
