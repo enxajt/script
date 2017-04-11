9181192aa8465aa4b403016ff6da5025706e947b
enxajt <enxajt@gmail.com>
2017-04-11 04:08:23 +0200

    sth

---
+++ set-proxy.sh
@@ -0,0 +1,6 @@
+# /bin/sh
+export http_proxy="http://***REMOVED***"
+export https_proxy="http://***REMOVED***"
+export HTTP_PROXY="http://***REMOVED***"
+export HTTPS_PROXY="http://***REMOVED***"
+export FTP_PROXY="http://***REMOVED***"
