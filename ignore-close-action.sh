04dd32561dcb7c3db2f1131f3c271191565b3302
enxajt <enxajt@gmail.com>
2017-07-01 23:08:13 +0900

    move init,install to init

--- ignore-close-action.sh
+++
@@ -1,6 +0,0 @@
-#Ubuntu 15.10 - Ubuntu 16.10
-#IgnoreLid=true in /etc/UPower/UPower.conf
-#sudoedit /etc/UPower/UPower.conf
-#Change IgnoreLid to IgnoreLid=true
-#Save and exit the editor.
-#service upower restart
--- init-jap-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# if this is true, a part of keyboard setting can be reset
-#gsettings set org.gnome.settings-daemon.plugins.keyboard active false
-
-# japanese input: 
-sudo apt-get remove -y ibus || echo nonexist?
