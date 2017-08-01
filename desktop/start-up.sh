da9cd8fb3adebc53c56992a8f36f73e37972c290
enxajt <enxajt@gmail.com>
2017-08-01 14:10:44 +0900

    sth

--- desktop/start-up.sh
+++ desktop/start-up.sh
@@ -9,12 +9,16 @@ setxkbmap -option "ctrl:swapcaps"
 xrandr --newmode "1600x900_60.00" 118.25 1600 1696 1856 2112 900 903 908 934 -hsync +vsync
 xrandr --addmode eDP-1 "1600x900_60.00"
 xrandr --output eDP-1 --mode "1600x900_60.00" 
+#rm $HOME/.config/monitors.xml
 
 #redshift -O 3600
 redshift -O 3000
 #redshift -x
 
-gnome-terminal -e 'tmux -2'
+# color is strange in tmux nvim
+#gnome-terminal -e 'tmux -2'
+gnome-terminal
+
 wmctrl -r "Terminal" -b toggle,maximized_vert,maximized_horz
 #wmctrl -r "Terminal" -e 0,0,0,900,1000
 #wmctrl -r "Terminal" -e 0,0,0,1930,1100
