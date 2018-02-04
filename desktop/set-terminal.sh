2b30ef893a753378be9fdb70507113b54ed72050
enxajt <enxajt@gmail.com>
2018-02-04 23:39:44 +0900

    sth

--- desktop/set-terminal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-link=/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9
-
-dconf write $link/use-transparent-background false
-#dconf write $link/use-transparent-background true
-#dconf write $link/background-transparency-percent '20'
-#dconf write $link/background-transparency-percent '5'
-
-#dconf write $link/cursor-shape 'ibeam'
--- desktop/vimfx/frame.js
+++
@@ -1,8 +0,0 @@
-vimfx.listen('getSelection', (willRemove, callback) => {
-  let selection = content.getSelection();
-  let s = selection.toString();
-  if (willRemove) {
-    selection.removeAllRanges();
-  }
-  callback(s);
-});
