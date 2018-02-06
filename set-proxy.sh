2f7e3158b537cd4ab30b9d018d126f1a3ffb1390
enxajt <enxajt@gmail.com>
2018-02-06 17:35:50 +0900

    sth

--- set-proxy.sh
+++
@@ -1,6 +0,0 @@
-# /bin/sh
-export http_proxy="http://***REMOVED***"
-export https_proxy="http://***REMOVED***"
-export HTTP_PROXY="http://***REMOVED***"
-export HTTPS_PROXY="http://***REMOVED***"
-export FTP_PROXY="http://***REMOVED***"
--- unset-proxy.sh
+++
@@ -1,6 +0,0 @@
-# /bin/sh
-sudo rm -f /etc/profile.d/proxy.sh
-unset http_proxy
-unset https_proxy
-unset HTTP_PROXY
-unset HTTPS_PROXY
