d70da8128a15c75f1a60d6a3ee34367bb841e242
enxajt <enxajt@gmail.com>
2019-08-19 11:25:12 +0900

    OK

--- volume.sh
+++ volume.sh
@@ -4,35 +4,26 @@ bth=`hcitool con | grep 00:87:63:11:19:1D`
 
 if [ -z "$bth" ]
 then
-  foreground='#DD0000'
-  background='#003300'
-  status="NC_BTH"
+  #emoji='🔇' #mute
+  emoji='🔈' #low-volume
+  status="speaker"
 else
-  foreground='#c0c5ce'
+  emoji="🎧"
+  status="headphones"
+fi
+
+if [ $volume -gt 0 ] && [ $status == "speaker" ]
+then
+  emoji="🔊"
+  foreground='#000000'
+  background='#BB0000'
+else
+  foreground='#999999'
   background='#000000'
-  status="HBQ-Q13S"
 fi
-
-if [ $volume -gt 20 ]
-then
-foreground='#000000'
-background='#FF0000'
-elif [ $volume -gt 10 ]
-then
-foreground='#000000'
-background='#BB0000'
-elif [ $volume -gt 0 ]
-then
-foreground='#c0c5ce'
-background='#660000'
-else
-foreground='#c0c5ce'
-background='#000000'
-fi
-
-text="<span foreground='$foreground' background='$background'><b>""🔊$volume""</b></span>"
 
 # Full and short texts
+text="<span foreground='$foreground' background='$background'><b>""$emoji$volume""</b></span>"
 echo "$text"
 echo "$text"
 
