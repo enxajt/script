fb0cebdd406a15eb570458e7ca7acaeab6fd315a
enxajt <enxajt@gmail.com>
2023-08-04 22:52:00 +0900

    init

---
+++ wifi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "wifi"
+echo -n "SSID: "
+read -r ssid
+echo -n "PSK: "
+read -r psk
+
+nmcli c add type wifi con-name "$ssid" ifname wlp2s0 ssid "$ssid"
+nmcli c modify "$ssid" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "%psk"
+nmcli c up "$ssid"
