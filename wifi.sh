5a455ec0fd7834cd1039c1ffeaefc0c0eae93e6e
enxajt <enxajt@gmail.com>
2023-08-18 16:40:02 +0900

    sth

--- wifi.sh
+++ wifi.sh
@@ -6,6 +6,6 @@ read -r ssid
 echo -n "PSK: "
 read -r psk
 
-nmcli c add type wifi con-name "$ssid" ifname wlp2s0 ssid "$ssid"
-nmcli c modify "$ssid" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "%psk"
-nmcli c up "$ssid"
+nmcli c add type wifi con-name $ssid ifname wlp2s0 ssid $ssid
+nmcli c modify $ssid wifi-sec.key-mgmt wpa-psk wifi-sec.psk $psk
+nmcli c up $ssid
